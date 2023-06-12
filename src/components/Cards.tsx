39e59700655e72a1cc990ca54ee81b8ebf65f195
GonzaloTuset <110003160+GonzaloTuset@users.noreply.github.com>
2023-06-12 16:40:57 -0300

    Update Cards.tsx

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -11,7 +11,7 @@ const Cards = ({ title, url, className }: Props) => {
       <a href={url} className='w-auto h-auto md:shadow md:shadow-late-400 rounded-sm text-black italic text-3xl flex flex-row md:flex-col'>
         <div className="w-48 h-12 md:pr-1 md:w-32 md:h-16 z-10 flex flex-row items-center">
           <p className={`${className} object-cover h-8 md:w-full md:ml-14 md:mt-4`}>{title}</p>
-          <img className='h-[40px]' src={Arrow} alt="flecha"></img>
+          <img className='h-[15px] ml-[10px] mt-[10px]' src={Arrow} alt="flecha"></img>
         </div>
         
       </a>
