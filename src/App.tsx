935ca426b7512d00159868903a6cd3b6d41d1741
GonzaloTuset <110003160+GonzaloTuset@users.noreply.github.com>
2023-06-12 19:15:32 -0300

    mas cambios

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
-import Form from "./components/Form"
 import image from './assets/ari editada.jpg'
 import Cards from "./components/Cards"
 import header from './assets/Fondo.jpeg'
-import Logo from './assets/OnlyFansLogo.png'
-import TikTok from './assets/ari tik tok,jpeg.jpg'
-import FondoTik from './assets/tik tok.png'
-import imgOnly from './assets/only.jpg'
-
+import './index.css'
 function App() {
   return (
-    <main className='bg-slate-800 w-screen h-screen text-white flex justify-center items-center'>
+    <main className='bg-white w-screen h-screen text-white flex justify-center items-center '>
       <img 
         src={header} 
         alt="Img" 
-        className="md:hidden absolute bg-cover inset-0 w-full h-full object-cover" 
+        className="blur-custom opacity-80  md:hidden absolute bg-cover inset-0 w-full h-full object-cover" 
       />
-      <article className='w-11/12 h-5/6 md:bg-slate-900 md:w-8/12 md:h-auto md:shadow-2xl drop-shadow-2xl shadow-slate-400 rounded-md flex'>
+
+      
+      <article className='w-11/12 h-full md:bg-slate-900 md:w-8/12 md:h-auto md:shadow-2xl drop-shadow-2xl shadow-slate-400 rounded-md flex'>
         <div className="hidden md:flex md:w-4/5 md:h-6/12 relative">
           <img 
             src={image} 
@@ -23,21 +20,38 @@ function App() {
             className="absolute bg-cover inset-0 w-full h-full object-cover rounded-l-md" 
           />
         </div>
+       
         
-        <div className='w-full h-auto md:h-[732px] flex flex-col justify-around items-center md:p-4'>
-          <div className=" flex flex-col mb-1 md:flex md:flex-row  md:items-center">
-            <div className='bg-pink-400 bg-opacity-70 rounded  md:mr-2.5'>
+        <div className='w-full h-auto md:h-[732px] flex flex-col  md:p-4'>
+        <h1 className='h1 '>Desireé</h1>
+        <p className='mt-[12px] p'>10.500 Followers</p>
+          <div className=" flex flex-col  md:flex md:flex-row  md:items-center">
+            <div className='bg-emerald-200 bg-opacity-70 h-[60px] w-[60px] rounded-full md:mr-2.5 mt-3 mb-3'>
               <Cards 
                 title={"OnlyFans"} 
                 subtitle="Desireé" 
                 url="https://onlyfans.com/arifeernandez"
               />
             </div>
-            <div className="bg-white bg-opacity-70 rounded  mb-2.5 md:mr-2.5">
+            <div className="bg-emerald-200 bg-opacity-70 h-[60px] w-[60px] rounded-full md:mr-2.5 mt-3 mb-3">
               <Cards 
-                title={FondoTik}
+                title={"TikTok"}
                 subtitle="Desireé" 
                 url="https://www.tiktok.com/@arifernandez__1?_t=8cTe4T0GOeL&_r=1"
+              />
+            </div>
+            <div className="bg-emerald-200 bg-opacity-70 h-[60px] w-[60px] rounded-full md:mr-2.5 mt-3 mb-3">
+              <Cards 
+                title={"Instagram"}
+                subtitle="Desireé" 
+                url="https://www.instagram.com/ari.feernandez/?hl=es-la"
+              />
+            </div>
+            <div className="bg-emerald-200 bg-opacity-70 h-[60px] w-[60px] rounded-full md:mr-2.5 mt-3 mb-3">
+              <Cards 
+                title={"Contactame"}
+                subtitle="Desireé" 
+                url="mailto:AriadnaDesiree1@gmail.com"
               />
             </div>
           </div>
