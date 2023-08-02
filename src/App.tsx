86f5808549aaa017c462744c96f09c81e6ceaf72
GonzaloTuset <110003160+GonzaloTuset@users.noreply.github.com>
2023-08-02 18:46:56 -0300

    dandole forma al pc

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import image from './assets/ari editada.jpg'
 import Cards from "./components/Cards"
 import header from './assets/Fondo.jpeg'
+
 import './index.css'
 
 function App() {
   return (
-    <main className='bg-white w-screen h-screen text-white flex justify-center items-center md:bg-blue-800'>
+    <main className={`bg-white w-screen h-screen text-white flex justify-center items-center md:bg-[url('./assets/fondoAri.png')]`}>
       <img 
         src={header} 
         alt="Img" 
@@ -27,34 +28,45 @@ function App() {
         <h1 className='h1 mt-1'>Ari Fernandez</h1>
         <p className='mt-[10px] p'>Mis redes Sociales:</p>
           <div className=" flex flex-col    ">
-            <div className='bg-emerald-200 bg-opacity-70 h-[60px] w-[60px] rounded-full md:mr-2.5 mt-3 mb-3'>
-              <Cards 
-                title={"OnlyFans"} 
-                subtitle="Desireé" 
-                url="https://onlyfans.com/arifeernandez"
-              />
-            </div>
-            <div className="bg-emerald-200 bg-opacity-70 h-[60px] w-[60px] rounded-full md:mr-2.5 mt-3 mb-3 md:circle">
-              <Cards 
-                title={"TikTok"}
-                subtitle="Desireé" 
-                url="https://www.tiktok.com/@arifernandez__1?_t=8cTe4T0GOeL&_r=1"
-              />
-            </div>
-            <div className="bg-emerald-200 bg-opacity-70 h-[60px] w-[60px] rounded-full md:mr-2.5 mt-3 mb-3">
-              <Cards 
-                title={"Instagram"}
-                subtitle="Desireé" 
-                url="https://www.instagram.com/ari.feernandez/?hl=es-la"
-              />
-            </div>
-            <div className="bg-emerald-200 bg-opacity-70 h-[60px] w-[60px] rounded-full md:mr-2.5 mt-3 mb-3">
-              <Cards 
-                title={"Contactame"}
-                subtitle="Desireé" 
-                url="mailto:AriadnaDesiree1@gmail.com"
-              />
-            </div>
+          <div className="flex flex-col md:flex-row md:w-4/5 md:h-6/12 relative">
+  <img 
+    src={image} 
+    alt="Img" 
+    className="absolute bg-cover inset-0 w-full h-full object-cover rounded-l-md md:rounded-l-none md:rounded-tl-md" 
+  />
+</div>
+
+<div className="bg-emerald-200 bg-opacity-70 h-[60px] w-[60px] rounded-full md:mr-2.5 mt-3 mb-3 md:circle">
+  <Cards 
+    title={"OnlyFans"} 
+    subtitle="Desireé" 
+    url="https://onlyfans.com/arifeernandez"
+  />
+</div>
+
+<div className="bg-emerald-200 bg-opacity-70 h-[60px] w-[60px] rounded-full md:mr-2.5 mt-3 mb-3 md:circle">
+  <Cards 
+    title={"TikTok"}
+    subtitle="Desireé" 
+    url="https://www.tiktok.com/@arifernandez__1?_t=8cTe4T0GOeL&_r=1"
+  />
+</div>
+
+<div className="bg-emerald-200 bg-opacity-70 h-[60px] w-[60px] rounded-full md:mr-2.5 mt-3 mb-3 md:circle">
+  <Cards 
+    title={"Instagram"}
+    subtitle="Desireé" 
+    url="https://www.instagram.com/ari.feernandez/?hl=es-la"
+  />
+</div>
+
+<div className="bg-emerald-200 bg-opacity-70 h-[60px] w-[60px] rounded-full md:mr-2.5 mt-3 mb-3 md:circle">
+  <Cards 
+    title={"Contactame"}
+    subtitle="Desireé" 
+    url="mailto:AriadnaDesiree1@gmail.com"
+  />
+</div>
           </div>
           {/*<Form/>*/}
         </div>
